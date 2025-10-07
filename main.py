4cee5f838730ca5eb67eceec9fec7aff837a929d
Moacir Júnior <mjunin1010@gmail.com>
2025-10-06 21:20:19 -0300

    Ajustes/Testes

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
-# %%
 import pandas as pd
 
 # Ajustes de exibição
-pd.set_option('display.max_rows', None)   # mostra todas as linhas
-pd.set_option('display.max_columns', None)  # mostra todas as colunas
-pd.set_option('display.width', None)     # evita quebra de linha
-pd.set_option('display.max_colwidth', None)  # mostra o conteúdo inteiro da célula
+pd.set_option("display.max_rows", None)   
+pd.set_option("display.max_columns", None) 
+pd.set_option("display.expand_frame_repr", None)
+pd.set_option("display.width", None)     
+#pd.set_option('display.max_colwidth', None)  
 
-# Carregar e exibir
-tabela = pd.read_excel("bigData.xlsx")
-tabela
-# %%
+abas = pd.read_excel("bigData.xlsx", sheet_name=None)
+
+#print(abas.keys())
+
+df_aba1 = abas["RELATORIO 2022"]
+df_aba2 = abas["RELATORIO 2023"]
+df_aba3 = abas["RELATORIO 2024"]
+df_aba4 = abas["RELATORIO 2025"]
+
+#print(df_aba1, df_aba2, df_aba3, df_aba4)
+
+df_completo = pd.concat(abas.values(), ignore_index=True)
+
+#print(df_completo)
+
+#numero_itens = len("bigData.xlsx")
+
+print(df_completo[["Funcionário", "Função", "Data do Exame", "Responsável ASO"]])
