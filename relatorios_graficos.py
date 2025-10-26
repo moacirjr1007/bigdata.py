5088c0830ef06a4ec87bf4a2ca5dc157ef51d8ff
Moacir Júnior <mjunin1010@gmail.com>
2025-10-25 23:19:12 -0300

    Junção dos relatórios e gráficos, agora ao invés de arquivos são funções, onde o flask consegue acessar-los

---
+++ relatorios_graficos.py
@@ -0,0 +1,38 @@
+import pandas as pd
+
+arquivo_excel = "bigData.xlsx"
+
+def gerar_relatorio_completo():
+    """Lê todas as abas, junta, limpa qualquer NaN e salva."""
+    try:
+        abas = pd.read_excel(arquivo_excel, sheet_name=None)
+        relatorio_completo = pd.concat(abas.values(), ignore_index=True)
+        
+        relatorio_final = relatorio_completo.dropna() 
+
+        nome_arquivo_saida = "Relatorio_Completo.xlsx"
+        relatorio_final.to_excel(nome_arquivo_saida, index=False)
+
+        return nome_arquivo_saida
+    except Exception as e:
+        print(f"Erro ao gerar relatório completo: {e}")
+        return None
+        
+def gerar_relatorio_funcionarios():
+    try:
+        abas = pd.read_excel(arquivo_excel, sheet_name=None)
+        relatorio_completo = pd.concat(abas.values(), ignore_index=True)
+        
+        relatorio_final = relatorio_completo.dropna(subset=["Funcionário", "Data do Exame"])
+
+        relatorio_exibicao = relatorio_final[["Funcionário", "Data do Exame"]]
+
+        nome_arquivo_saida = "Relatorio_Funcionarios.xlsx" 
+
+        relatorio_exibicao.to_excel(nome_arquivo_saida, index=False)
+
+        return nome_arquivo_saida
+    except Exception as e:
+
+        print(f"Erro ao gerar relatório de funcionários: {e}")
+        return None
